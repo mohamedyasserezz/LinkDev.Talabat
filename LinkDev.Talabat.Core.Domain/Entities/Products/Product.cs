b683dd7af25faf1bfd094a646aacaf5da17d4727
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2024-10-05 20:07:37 +0300

    Part 03 Product Module - Entities

---
+++ LinkDev.Talabat.Core.Domain/Entities/Products/Product.cs
@@ -0,0 +1,22 @@
+﻿using LinkDev.Talabat.Core.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.Talabat.Core.Domain.Entities.Products
+{
+    public class Product : BaseEntity<int>
+    {
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+        public string? PictureUrl { get; set; }
+        public decimal Price { get; set; }
+        public int? BrandId { get; set; } // FK --> for Product and Brand Relationship
+        public int? CategoryId { get; set; } // FK --> for Product and Category Relationship
+        public virtual ProductBrand? Brand { get; set; }
+        public virtual ProductCategory? Category { get; set; }
+
+    }
+}
