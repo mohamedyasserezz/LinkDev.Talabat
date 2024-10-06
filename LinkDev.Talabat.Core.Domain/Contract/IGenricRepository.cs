13d0fbe1c15378b3e615e874d50c3555a3597d33
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2024-10-06 20:55:50 +0300

    Part 08 Generic Repository

---
+++ LinkDev.Talabat.Core.Domain/Contract/IGenricRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.Talabat.Core.Domain.Contract
+{
+    public interface IGenricRepository<TEntity,TKey> 
+        where TEntity : BaseEntity<TKey>
+        where TKey : IEquatable<TKey>
+    {
+        public Task<IEnumerable<TEntity>> GetAllAsync(bool withTracking);
+        public Task<TEntity?> GetAsync(TKey id);
+        public Task AddAsync(TEntity entity);
+        public void Update(TEntity entity); 
+        public void Delete(TEntity entity);
+    }
+}
