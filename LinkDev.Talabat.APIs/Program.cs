ab1535302c5e477382dccc5aed65cebdf386bd0c
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2024-10-06 03:08:42 +0300

    Part 05 Apply Migrations (Update-Database)

--- LinkDev.Talabat.APIs/Program.cs
+++ LinkDev.Talabat.APIs/Program.cs
@@ -1,10 +1,12 @@
 using LinkDev.Talabat.Infrastructure.Persistence;
+using LinkDev.Talabat.Infrastructure.Persistence.Data;
+using Microsoft.EntityFrameworkCore;
 namespace LinkDev.Talabat.APIs
 {
     public class Program
     {
         // Entry Point
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
@@ -22,11 +24,39 @@ namespace LinkDev.Talabat.APIs
             // {
             //     optionsBuilder.UseSqlServer(builder.Configuration.GetConnectionString("StoreContext"));
             // });
-             builder.Services.AddPersistanceServices(builder.Configuration);
+            builder.Services.AddPersistanceServices(builder.Configuration);
             #endregion
 
             var app = builder.Build();
 
+            
+            #region Update Database
+            using var scope = app.Services.CreateAsyncScope();
+            var Services = scope.ServiceProvider;
+            var storeContext = Services.GetRequiredService<StoreContext>();
+
+            var loggerFactory = Services.GetRequiredService<ILoggerFactory>();
+
+            try
+            {
+                var PendingMigrations = await storeContext.Database.GetPendingMigrationsAsync();
+
+                if (PendingMigrations.Any())
+                    await storeContext.Database.MigrateAsync();
+            }
+            catch (Exception ex)
+            {
+                var logger = loggerFactory.CreateLogger<Program>();
+                logger.LogError(ex, ex.Message);
+            }
+            finally
+            {
+                // scope.Dispose();
+                await storeContext.DisposeAsync();
+            }
+
+            #endregion
+             
             #region Configure Kestrel Middlewares
             // Configure the HTTP request pipeline.
 
@@ -40,7 +70,7 @@ namespace LinkDev.Talabat.APIs
 
 
 
-            app.MapControllers(); 
+            app.MapControllers();
             #endregion
 
             app.Run();
