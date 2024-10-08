fccdbc4e944f020638150776d1d8b73d6bb0cdf9
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2024-10-09 01:45:47 +0300

    Part03 refactor BaseEntity Configuration

---
+++ LinkDev.Talabat.Infrastructure.Persistence/Data/Config/Base/BaseAudititbleEntityConfigurations.cs
@@ -0,0 +1,28 @@
+﻿using LinkDev.Talabat.Core.Domain.Common;
+
+
+namespace LinkDev.Talabat.Infrastructure.Persistence.Data.Config.Base
+{
+    internal class BaseAudititbleEntityConfigurations<TEntity, TKey> : BaseEntityConfigurations<TEntity,TKey>
+        where TEntity : BaseAuditableEntity<TKey>
+        where TKey : IEquatable<TKey>
+    {
+        public override void Configure(EntityTypeBuilder<TEntity> builder)
+        {
+            
+            builder.Property(E => E.CreatedBy)
+                .IsRequired();
+
+            builder.Property(E => E.CreatedOn)
+                .IsRequired()
+                /*.HasDefaultValueSql("GetUTCDate()")*/;
+
+            builder.Property(E => E.LastModifiedBy)
+                .IsRequired();
+
+            builder.Property(E => E.LastModifiedOn)
+               .IsRequired()
+               /*.HasDefaultValueSql("GetUTCDate()")*/;
+        }
+    }
+}
--- LinkDev.Talabat.Infrastructure.Persistence/Data/Config/Base/BaseEntityConfigurations.cs
+++ LinkDev.Talabat.Infrastructure.Persistence/Data/Config/Base/BaseEntityConfigurations.cs
@@ -1,31 +1,20 @@
 ﻿using LinkDev.Talabat.Core.Domain.Common;
-
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace LinkDev.Talabat.Infrastructure.Persistence.Data.Config.Base
 {
     internal class BaseEntityConfigurations<TEntity, TKey> : IEntityTypeConfiguration<TEntity>
-        where TEntity : BaseAuditableEntity<TKey>
+        where TEntity : BaseEntity<TKey>
         where TKey : IEquatable<TKey>
     {
         public virtual void Configure(EntityTypeBuilder<TEntity> builder)
         {
             builder.Property(E => E.Id)
                .ValueGeneratedOnAdd();
-
-
-            builder.Property(E => E.CreatedBy)
-                .IsRequired();
-
-            builder.Property(E => E.CreatedOn)
-                .IsRequired()
-                /*.HasDefaultValueSql("GetUTCDate()")*/;
-
-            builder.Property(E => E.LastModifiedBy)
-                .IsRequired();
-
-            builder.Property(E => E.LastModifiedOn)
-               .IsRequired()
-               /*.HasDefaultValueSql("GetUTCDate()")*/;
         }
     }
 }
