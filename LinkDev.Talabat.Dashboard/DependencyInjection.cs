04c677eada3b8eb2c0724ec283222c13162534fc
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2024-10-26 01:37:00 +0300

    Part 01

---
+++ LinkDev.Talabat.Dashboard/DependencyInjection.cs
@@ -0,0 +1,60 @@
+﻿using LinkDev.Talabat.Core.Domain.Entities.Identity;
+using LinkDev.Talabat.Infrastructure.Persistence.Data;
+using LinkDev.Talabat.Infrastructure.Persistence.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using StackExchange.Redis;
+
+namespace LinkDev.Talabat.Dashboard
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddDashboardServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<StoreDbContext>(optionsBuilder =>
+            {
+                optionsBuilder
+                .UseSqlServer(configuration.GetConnectionString("StoreContext"))
+                .UseLazyLoadingProxies();
+            });
+
+          
+
+            services.AddDbContext<StoreIdentityDbContext>(optionsBuilder =>
+            {
+                optionsBuilder
+                .UseLazyLoadingProxies()
+                .UseSqlServer(configuration.GetConnectionString("IdentityContext"));
+            });
+
+            services.AddIdentity<ApplicationUser, IdentityRole>(identityOptions =>
+            {
+                identityOptions.User.RequireUniqueEmail = true;
+
+                identityOptions.SignIn.RequireConfirmedAccount = true;
+                identityOptions.SignIn.RequireConfirmedEmail = true;
+                identityOptions.SignIn.RequireConfirmedPhoneNumber = true;
+
+
+                /// identityOptions.Password.RequireNonAlphanumeric = true;
+                /// identityOptions.Password.RequiredUniqueChars = 2;
+                /// identityOptions.Password.RequiredLength = 6;
+                /// identityOptions.Password.RequireDigit = true;
+                /// identityOptions.Password.RequireLowercase = true;
+                /// identityOptions.Password.RequireUppercase = true;
+
+
+                identityOptions.Lockout.AllowedForNewUsers = true;
+                identityOptions.Lockout.MaxFailedAccessAttempts = 10;
+                identityOptions.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(10);
+
+
+                //identityOptions.Stores
+                //identityOptions.ClaimsIdentity
+                //identityOptions.Tokens
+            })
+               .AddEntityFrameworkStores<StoreIdentityDbContext>();
+            return services;
+        }
+    }
+}
--- LinkDev.Talabat.Dashboard/Program.cs
+++ LinkDev.Talabat.Dashboard/Program.cs
@@ -9,6 +9,9 @@ namespace LinkDev.Talabat.Dashboard
             // Add services to the container.
             builder.Services.AddControllersWithViews();
 
+            builder.Services.AddDashboardServices(builder.Configuration);
+
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
